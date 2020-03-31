f212a3504acfedacb6e2102f0e21830781aed1c9
Ole Michaelis <Ole.Michaelis@googlemail.com>
2020-03-31 15:46:52 +0200

    bump to v2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HPack.Mixfile do
   def project do
     [
       app: :hpack,
-      version: "1.1.0",
+      version: "2.0.0",
       elixir: "~> 1.9",
       package: package(),
       build_embedded: Mix.env() == :prod,
